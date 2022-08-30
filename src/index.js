2791c013aeda9a18a6604c690360e6ce2057d215
technosheen <info@technosheen.com>
2022-08-30 11:11:45 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import $ from "jquery";
+
+const $showData = $("#show-data");
+const $raw = $("pre");
+
+$("#get-data").on("click", (e) => {
+  e.preventDefault();
+
+  $showData.text("Loading the JSON file.");
+
+  $.getJSON(
+    "https://recruiting.paylocity.com/recruiting/v2/api/feed/jobs/24da04f3-750a-4cea-a65e-bea4cec1002e",
+    (data) => {
+      console.log(data);
+      const markup = data.jobs
+        .map((job) => `<li>${job.title}: ${job.jobLocation.name}</li>`)
+        .join("");
+
+      const list = $("<ul />").html(markup);
+
+      $showData.html(list);
+
+      $raw.text(JSON.stringify(data, undefined, 2));
+    }
+  );
+});
